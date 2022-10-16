3ef309eb720be9d1c37e9d04989aae29f4f62744
Sebastian Dalfuß <git@sedf.de>
2022-10-17 00:01:06 +0200

    Implemented geometry for 9-tree

--- nontree/NonTree.py
+++ nontree/NonTree.py
@@ -87,7 +87,7 @@ class NonTree:
                     # push to lower left
                     self.subtrees[2].push(coord, value)
             else:
-                if coord[1] < self.subtrees[3].rect[1]:  # y:
+                if coord[1] < self.subtrees[2].rect[1]:  # y:
                     # push to upper right
                     self.subtrees[1].push(coord, value)
                 else:
@@ -95,7 +95,36 @@ class NonTree:
                     self.subtrees[3].push(coord, value)
 
         elif self.mode == 9:
-            pass  # TODO
+            if coord[0] < self.subtrees[1].rect[0]:
+                if coord[1] < self.subtrees[3].rect[1]:
+                    # push to upper left
+                    self.subtrees[0].push(coord, value)
+                elif coord[1] < self.subtrees[6].rect[1]:
+                    # push to upper middle
+                    self.subtrees[3].push(coord, value)
+                else:
+                    # push to upper right
+                    self.subtrees[6].push(coord, value)
+            elif coord[0] < self.subtrees[2].rect[0]:
+                if coord[1] < self.subtrees[3].rect[1]:
+                    # push to middle left
+                    self.subtrees[1].push(coord, value)
+                elif coord[1] < self.subtrees[6].rect[1]:
+                    # push to middle middle
+                    self.subtrees[4].push(coord, value)
+                else:
+                    # push to middle right
+                    self.subtrees[7].push(coord, value)
+            else:
+                if coord[1] < self.subtrees[3].rect[1]:
+                    # push to lower left
+                    self.subtrees[2].push(coord, value)
+                elif coord[1] < self.subtrees[6].rect[1]:
+                    # push to lower middle
+                    self.subtrees[5].push(coord, value)
+                else:
+                    # push to lower right
+                    self.subtrees[8].push(coord, value)
 
     def __split(self):  # add subtrees
         x, y, width, height = self.rect
@@ -116,8 +145,6 @@ class NonTree:
 
             self.subtrees.append(NonTree((x, y, w0, height), self.mode, self.use_sets))
             self.subtrees.append(NonTree((x1, y, w1, height), self.mode, self.use_sets))
-
-            pass  # TODO
 
         elif self.mode == 4:
             # Sector layout
@@ -156,4 +183,58 @@ class NonTree:
             # [0][1][2]
             # [3][4][5]
             # [6][7][8]
-            pass  # TODO
+
+            # x0 = x
+            # y0 = y
+            w0 = width // 3
+            h0 = height // 3
+
+            x1 = x + w0  # x1 = x0 + w0
+            # y1 = y0
+            # w1 = w0
+            # h1 = h0
+
+            x2 = x + 2 * w0  # x2 = x0 + 2 * w0
+            # y2 = y0
+            w2 = width - 2 * w0
+            # h2 = h0
+
+            # x3 = x0
+            y3 = y + h0  # y3 = y0 + h0
+            # w3 = w0
+            # h3 = h0
+
+            # x4 = x1
+            # y4 = y3
+            # w4 = w0
+            # h4 = h0
+
+            # x5 = x2
+            # y5 = y3
+            # w5 = w2
+            # h5 = h0
+
+            # x6 = x0
+            y6 = y + 2 * h0  # y6 = y0 + 2 * h0
+            # w6 = w0
+            h6 = height - 2 * h0
+
+            # x7 = x1
+            # y7 = y6
+            # w7 = w0
+            # h7 = h6
+
+            # x8 = x2
+            # y8 = y6
+            # w8 = w2
+            # h8 = h6
+
+            self.subtrees.append(NonTree((x, y, w0, h0), self.mode, self.use_sets))
+            self.subtrees.append(NonTree((x1, y, w0, h0), self.mode, self.use_sets))
+            self.subtrees.append(NonTree((x2, y, w2, h0), self.mode, self.use_sets))
+            self.subtrees.append(NonTree((x, y3, w0, h0), self.mode, self.use_sets))
+            self.subtrees.append(NonTree((x1, y3, w0, h0), self.mode, self.use_sets))
+            self.subtrees.append(NonTree((x2, y3, w2, h0), self.mode, self.use_sets))
+            self.subtrees.append(NonTree((x, y6, w0, h6), self.mode, self.use_sets))
+            self.subtrees.append(NonTree((x1, y6, w0, h6), self.mode, self.use_sets))
+            self.subtrees.append(NonTree((x2, y6, w2, h6), self.mode, self.use_sets))
